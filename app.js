033056ffa9b41b21ab1b5dcbc4c4bb4be2da5aa6
Otba Al-Habahbeh <79699953+Otba-02@users.noreply.github.com>
2023-03-07 20:35:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,29 +9,38 @@ Vue.createApp({
       counterRoundSpecialAttack: 0,
       counterRoundHeal: 0,
       result: null,
+      battleLog: [],
     };
   },
   computed: {
     playerHealthBar() {
+      if (this.playerHealth < 0) {
+        this.playerHealth = 0;
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
     },
     monsterHealthBar() {
+      if (this.monsterHealth < 0) {
+        this.monsterHealth = 0;
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth / 1.5 + "%" };
     },
   },
   watch: {
-    monsterHealth() {
-      if (this.monsterHealth > 0 && this.playerHealth <= 0) {
-        this.result = "lose";
-      } else if (this.monsterHealth <= 0 && this.playerHealth <= 0) {
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
         this.result = "draw";
+      } else if (value <= 0) {
+        this.result = "win";
       }
     },
-    playerHealth() {
-      if (this.playerHealth > 0 && this.monsterHealth <= 0) {
-        this.result = "win";
-      } else if (this.monsterHealth <= 0 && this.playerHealth <= 0) {
+    playerHealth(value) {
+      if (this.monsterHealth <= 0 && value <= 0) {
         this.result = "draw";
+      } else if (value <= 0) {
+        this.result = "lose";
       }
     },
   },
@@ -42,39 +51,47 @@ Vue.createApp({
       this.counterRoundSpecialAttack = 0;
       this.counterRoundHeal = 0;
       this.result = null;
+      this.battleLog = [];
     },
     attackMonester() {
-      this.monsterHealth -= getRandomNumber(10, 12);
-      if (this.monsterHealth <= 0) {
-        this.monsterHealth = 0;
-      }
+      const attackValue = getRandomNumber(10, 12);
+      this.monsterHealth -= attackValue;
+      this.getBattleLog("player", "attack", attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
-      this.playerHealth -= getRandomNumber(7, 16);
-      if (this.playerHealth <= 0) {
-        this.playerHealth = 0;
-      }
+      const attackValue = getRandomNumber(7, 16);
+      this.playerHealth -= attackValue;
+      this.getBattleLog("monster", "attack", attackValue);
     },
     specialAttack() {
       this.counterRoundSpecialAttack++;
-      this.monsterHealth -= getRandomNumber(25, 30);
-      if (this.monsterHealth <= 0) {
-        this.monsterHealth = 0;
-      }
+      const attackValue = getRandomNumber(25, 30);
+      this.monsterHealth -= attackValue;
+      this.getBattleLog("player", "attack", attackValue);
       this.attackPlayer();
     },
     healPlayer() {
       if (this.playerHealth < 100) {
         this.counterRoundHeal++;
-        this.playerHealth += getRandomNumber(20, 35);
-        if (this.playerHealth > 100) {
+        const healValue = getRandomNumber(20, 35);
+        this.getBattleLog("player", "heal", healValue);
+        if (this.playerHealth + healValue > 100) {
           this.playerHealth = 100;
+        } else {
+          this.playerHealth += healValue;
         }
       }
     },
     surrendPlayer() {
       this.result = "lose";
+    },
+    getBattleLog(who, what, value) {
+      this.battleLog.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 }).mount("#game");
